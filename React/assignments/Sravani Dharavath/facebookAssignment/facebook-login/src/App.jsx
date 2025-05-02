58d3c5df16e013c764b572de5a9acb50f5562141
sravanidharavath332@gmail.com <sravanidharavath332@gmail.com>
2025-05-02 22:26:37 +0500

    added counterApp,dogFetchApi,FacebookJSON

---
+++ React/assignments/Sravani Dharavath/facebookAssignment/facebook-login/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUpForm from "./components/SignUpForm";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default App;
